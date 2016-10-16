115c6f6083d6bcec80c20446a75e16a3af186b78
vschmidt <vschmidt@ccom.unh.edu>
2016-10-15 21:51:13 -0400

    The script now takes arguments that allow one to specify which logs to parse and extact, (or all of them) and to specify the input and output directories as necessary. Also fixes to better handle spaces in path or file names.

--- bash/cworker_logextractor.sh
+++ bash/cworker_logextractor.sh
@@ -9,46 +9,128 @@
 # TO DO:
 # Add the ability to export all log directories.
 
-if [ $1 == "-h" ]; then
+DOCW4=0
+DOCSV=0
+DO2000=0
+DO0183=0
+PRINTHELP=0
+GOTOUTPUT=0
+
+while getopts ":rewqhd:o:" opt; do
+
+    case $opt in 
+	r) 
+	    DOCW4=1
+	    ;;
+	e)
+	    DOCSV=1
+	    ;;
+	w)
+	    DO2000=1
+	    ;;
+	q)
+	    DO0183=1
+	    ;;
+	d) 
+	    ccscm=${OPTARG}
+	    ;;
+	o)  
+	    GOTOUTPUT=1
+	    outputdir=${OPTARG}/extracted_logs
+	    ;;
+	h)
+	    PRINTHELP=1
+	    ;;
+	a) 
+	    DOCW4=1
+	    DOCSV=1
+	    DO2000=1
+	    DO0183=1
+	    ;;
+	\?)
+	    echo "Invalid option: $OPTARG" >&2
+	    exit 1
+	    ;;
+	:)
+	    echo "Option -$OPTARG requires an argument." >&2
+	    exit 1
+	    ;;
+    esac
+
+done
+
+echo $DOCW4
+echo $DOCSV
+echo $DO2000
+echo $DO0183
+echo $PRINTHELP
+echo $ccscm
+echo $outputdir
+
+
+if [ "$PRINTHELP" == 1 ]; then
 
     echo ""
     echo "USAGE: "
-    echo "    asv_log_extractor.sh <path/to/ccscm> \ "
-    echo "           [/path/to/output/dir]"
-    echo " "
+    echo "    cworker_log_extractor.sh [-r|-e|-w|-q|-a] [-h] -d <path/to/ccscm> \ "
+    echo "           -o [/path/to/output/dir]"
+    echo ""
+    echo "  -r extracts the CW4 logs from their binary form to CSV files."
+    echo "  -e parses the CSV files, modifies the parameters names and produces .mat files. (calls parsecsv.py)"
+    echo "     NOTE: If -e is called without -r, it is assumed that the CW4 files are already parsed. "
+    echo "           In this case, the argument to -d should be /path/to/extracted_logs/"
+    echo "           and -o should probably be the same to put the results in the same directory."
+    echo "  -w extracts data from the nmea2000 log files. (calls parsenmea2000.py)"
+    echo "  -q extracts data from the nmea1830 log files. (calls parsenmea0183.py, which in turn calls gpsparser.py)"
+    echo "  -a does all of these. "
+    echo ""
+    echo ""
     echo "Logs will be extracted for the selected log directories"
     echo "into /path/to/output/dir/extracted_logs/ producing CSV"
     echo "files for each of the standard log types. If the output"
     echo "directory is omitted, the current working directory is used."
     echo ""
-    exit
+    exit 1
+fi
+
+
+if [ "$outputdir" == "" ]; then
+    echo "You must specify an output directory (-o <path>)." >&2
+    exit 1
 fi
 
 # An environment varialbe we'll eventually set.
 #ASVG_TOOLS="/home/asvuser/gitsrc/ASVG_tools/"
 
-# The first argument is the path to the ccscm log directory. 
-ccscm=$1
+if [ "$ASVG_TOOLS" = "" ]; then
+    echo "Set environment variable ASVG_TOOLS to the ASVG_tool/ directory."
+    echo "e.g. export ASVG_TOOLS=/path/to/ASVG_tool/"
+    exit 1
+fi
 
-if [ ! -e $ccscm ]; then
+# The first argument is the path to the ccscm log directory. 
+#ccscm=$1
+
+if [ ! -e "$ccscm" ]; then
     echo "Could not find $ccscm"
     echo "Exiting..."
-    exit
+    exit 
 fi 
 
-ccscm=`readlink -f "$ccscm"`
+
+#ccscm=`readlink -f "$ccscm"`
 
 # The second argument is the directory into which the extact data will
 # go.
-tmp=$2
+#tmp=$2
 
-if [ "$tmp" == "" ]; then
-    tmp=`pwd`
-    echo "Setting default output directory to "
-    echo "    $tmp/extracted_logs"
-fi
-outputdir="$tmp/extracted_logs/"
-outputdir=`realpath $outputdir`
+#if [ "$outputdir" == "" ]; then
+#    tmp=`pwd`
+#    echo "Setting default output directory to "
+#    echo "    $tmp/extracted_logs"
+#fi
+#outputdir="$tmp/extracted_logs/"
+outputdir=`realpath "$outputdir"`
 
 # TODO: Check to see that the output directory exist and either fail
 # or make it.   
@@ -57,18 +139,31 @@ outputdir=`realpath $outputdir`
 # yet clear what causes a rotation of logging directory by ASV. For
 # now logs are extracted on a directory basis. 
 
-alllogdirs=`ls -1 $ccscm/scm-vp`
+# In this case we have presumably already extracted CSV files,
+# we will have selected the "extract_logs" directory for input.
+if [ "$DOCSV" == "1" ] && [ "$DOCW4" == "0" ]; then
+    alllogdirs=`ls -1 ${ccscm}`
+else
+    alllogdirs=`ls -1 "${ccscm}/scm-vp"`
+fi
 
 # Get a list of the logs that actually have data and display them.
 logswithdata=()
 echo "I have detected logs in these directories:"
 for logdir in ${alllogdirs[@]}; do
-
-    if [ -e "$ccscm/scm-vp/$logdir/smState.smx" ]; then
-	logswithdata=( ${logswithdata[@]} $logdir )
-	echo "$logdir"
+    
+    if [ "$DOCW4" == "1" ] || [ "$DO2000" == "1" ] || [ "$DO0183" == "1" ]; then
+	
+	if [ -e "$ccscm/scm-vp/$logdir/smState.smx" ]; then
+	    logswithdata=( ${logswithdata[@]} $logdir )
+	    echo "$logdir"
+	fi
+    else
+	if [ -e "$ccscm/$logdir/engine.csv" ]; then
+	    logswithdata=( ${logswithdata[@]} $logdir )
+	    echo "$logdir"
+	fi
     fi
-
 done
 
 # Get the user's selection. 
@@ -86,10 +181,14 @@ while [ "$datadir" != 'q' ]; do
 	# directories that have data. Maybe some other checking is worth
 	# while here?
 	# echo "Checking $ccscm/$datadir..."
-	if [ -e "$ccscm/scm-vp/$datadir" ]; then
+	if [ "$DOCW4" == 1 ]; then
+	    if [ -e "$ccscm/scm-vp/$datadir" ]; then
                 datadirs=( "${datadirs[@]}" $datadir )
+	    else
+    		echo "That data directory does not exist. Try again. ('q' to quit)."
+	    fi
 	else
-    	    echo "That data directory does not exist. Try again. ('q' to quit)."
+	    datadirs=( "${datadirs[@]}" $datadir )
 	fi
 
 	read datadir
@@ -98,14 +197,17 @@ while [ "$datadir" != 'q' ]; do
 
 done
 
-if [ ! -e /usr/local/bin/data-export-cli ]; then
-    echo "Unable to find the data export tool: data-export-cli"
-    exit
+if [ "$DOCW4" == 1 ]; then
+    if [ ! -e /usr/local/bin/data-export-cli ]; then
+	echo "Unable to find the data export tool: data-export-cli"
+	exit
+    fi
 fi
 
 
 # Process the data. 
 for datadir in ${datadirs[@]}; do
+
 
     echo "Extracting ASV C-Worker Binary Logs..."
     # Define and create the output directory.
@@ -117,23 +219,24 @@ for datadir in ${datadirs[@]}; do
     # Capture where the command was executed from.
     cwd=`pwd`
 
+    if [ "$DOCW4" == 1 ]; then
     # A bug in data-export-cli utility will not support multiple
     # exports in a single file. So separate export configuration files
     # had to be generated for each. This line hails from a time when
     # we expected to be able to do it in a single go. 
     #cd "$complete_outputdir/configs"
-    ${ASVG_TOOLS}/bash/create_export_config.sh "$complete_outputdir" > "$outputspec"
-    echo "$outputspec"
+	${ASVG_TOOLS}/bash/create_export_config.sh "$complete_outputdir" > "$outputspec"
+	echo "$outputspec"
     # Go to the output directory and create the config files. 
     
     #create_export_configs.sh "$complete_outputdir" 
-    cd $complete_outputdir
+	cd $complete_outputdir
 
     # Then find all the config files we generated and process each. 
-    echo "Processing $datadir..."
+	echo "Processing $datadir..."
 
-    data-export-cli -d "$ccscm/scm-vp/$datadir" \
-     	-x "$outputspec" 2>&1 >> $complete_outputdir/export.log  
+	data-export-cli -d "$ccscm/scm-vp/$datadir" \
+     	    -x "$outputspec" 2>&1 >> $complete_outputdir/export.log  
 
     # In this block, separate processes were launched for each export 
     # and these were monitored for completion. 
@@ -158,17 +261,31 @@ for datadir in ${datadirs[@]}; do
     
     # done
 
-    echo ""
-    echo "Parsing CSV files for MATLAB."
-    ${ASVG_TOOLS}/pyasv/bin/parsecsv.py -d $complete_outputdir -o i 
-    echo ""
+    fi
 
-    echo ""
-    echo "Parsing nmea0183 logs..."
-    ${ASVG_TOOLS}/pyasv/bin/parsenmea0183.py -d $ccscm/scm-vp/$datadir -o $complete_outputdir 
-    echo ""
-    echo "Parsing nmea2000 logs..."
-    ${ASVG_TOOLS}/pyasv/bin/parsenmea2000.py -d $ccscm/scm-vp/$datadir -o $complete_outputdir 
+
+    if [ "$DOCSV" == 1 ]; then
+	echo ""
+	echo "Parsing CSV files for MATLAB."
+	if [ "$DOCW4" == 1 ]; then
+	    ${ASVG_TOOLS}/pyasv/bin/parsecsv.py -d \""${complete_outputdir}"\" -o i -vv
+	else
+	    ${ASVG_TOOLS}/pyasv/bin/parsecsv.py -d \""${ccscm}/${datadir}"\" -o \""${outputdir}/${datadir}"\" -vv
+	fi
+	echo ""
+    fi
+
+    if [ "$DO0183" = 1 ]; then 
+	echo ""
+	echo "Parsing nmea0183 logs..."
+	${ASVG_TOOLS}/pyasv/bin/parsenmea0183.py -d \""${ccscm}/scm-vp/${datadir}"\" -o \""${complete_outputdir}"\" 
+	echo ""
+    fi
+
+    if [ "$DO2000" = 1 ]; then
+	echo "Parsing nmea2000 logs..."
+	${ASVG_TOOLS}/pyasv/bin/parsenmea2000.py -d \""${ccscm}/scm-vp/${datadir}"\" -o \""${complete_outputdir}"\" 
+    fi
 
     echo ""
     echo "Export of $datadir complete."
@@ -176,6 +293,3 @@ for datadir in ${datadirs[@]}; do
     cd "$cwd"
     
 done
-
-
-
