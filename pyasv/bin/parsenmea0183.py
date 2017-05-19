0f6779f588a72eb39590d47bf901b9de9e37fc44
Unmanned User <unmanned@ui-01-080.asv.global>
2017-05-19 18:14:10 +0000

    Added ability to select device type to parse.

--- pyasv/bin/parsenmea0183.py
+++ pyasv/bin/parsenmea0183.py
@@ -31,40 +31,57 @@ parser.add_argument("-o", "--outputdir",
                     action = "store",
                     default = ".",
                     help = "Specify the output directory for parsed MATLAB files. [default='.']")
-
+parser.add_argument("-l",'--log_device',
+                    action = "store",
+                    default = 'a',
+                    help = 'Device index to parse (e.g. #.nmea0183)')
+                    
 # Get list of files to parse.
 args = parser.parse_args()
 directory =         args.directory
 dryrun =            args.dryrun
 verbose =           args.verbosity
 outputdir =         args.outputdir
+log_device =        args.log_device.split(',')
+
 
 ########################## NMEA 0183 ##################################        
 GPStypestoparse = ['GGA','RMC','VTG'] # Not currently parsing GSV or GLL
+NMEA_Devices = ['3','6']  # Hard coded options here. 3 is factory GPS, 6 is POSMV
+
+# Handle manually specified device to parse.
+if log_device[0] == 'a':
+    devices_to_parse = NMEA_Devices
+else:
+    devices_to_parse = log_device
+    
 
 thisdir = os.path.dirname(__file__)
 gpsparser = os.path.join(thisdir,'../lib/gpsparser/gpsparser/gpsparser.py')
+
 # Specify the output directory name.
 # Default is the cwd ('./'). -i gives the input directory. 
 # Note the grep removes non-ASCII characters, which come with the AIS logs.
-for logtype in GPStypestoparse:
-    cmd = ('/bin/cat ' + os.path.join(os.path.join(directory,'device'),'*nmea0183') 
-           + ' | strings | ' + gpsparser + ' -s ' + logtype + ' -o ' + 
-           os.path.join(outputdir,logtype + '.txt'))
-    #cmd = ('/bin/cat ' + os.path.join(os.path.join(directory,'device'),'*nmea0183') 
-    #       + ' | perl -pe \'s/[^[:ascii:]]//g\' | ' + gpsparser + ' -s ' + logtype + ' -o ' + 
-    #       os.path.join(outputdir,logtype + '.txt'))
-  
-   #cmd = (gpsparser + 
-    #       ' -g ' + os.path.join(directory,'device') + '::nmea0183' +
-    #' -s ' + logtype + ' -o ' + outputdir)
+# NOTE: Device 3 is the onboard GPS.
+for device in devices_to_parse:
+    for logtype in GPStypestoparse:
+        cmd = ('/bin/cat ' + os.path.join(os.path.join(directory,'device'),'*' + device + '.nmea0183') 
+               + ' | strings | ' + gpsparser + ' -s ' + logtype + ' -o ' + 
+               os.path.join(outputdir,logtype + '.txt'))
+               #cmd = ('/bin/cat ' + os.path.join(os.path.join(directory,'device'),'*nmea0183') 
+               #       + ' | perl -pe \'s/[^[:ascii:]]//g\' | ' + gpsparser + ' -s ' + logtype + ' -o ' + 
+               #       os.path.join(outputdir,logtype + '.txt'))
+               
+               #cmd = (gpsparser + 
+               #       ' -g ' + os.path.join(directory,'device') + '::nmea0183' +
+               #' -s ' + logtype + ' -o ' + outputdir)
    
-    if verbose >= 1:
-        print "Executing: " + cmd
+        if verbose >= 1:
+            print "Executing: " + cmd
         
-    if not dryrun:
-        try:
-            os.system(cmd)
-        except:
-            print "Failed executing: " + cmd
+        if not dryrun:
+            try:
+                os.system(cmd)
+            except:
+                print "Failed executing: " + cmd
             
