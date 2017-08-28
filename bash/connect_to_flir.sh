c5b77c55e9c092a8a1cb8faf3ce57561b55aa2c2
Unmanned User <unmanned@ui-01-080.asv.global>
2017-08-28 14:19:38 +0000

    Removed unnecessary sudo.

--- bash/connect_to_flir.sh
+++ bash/connect_to_flir.sh
@@ -5,5 +5,5 @@
 
 echo "Setting up connection to FLIR. Enter password at prompts."
 echo "Connect to FLIR on http://192.168.100.10:9000"
-ssh -e sudo socat TCP-LISTEN:9000,fork TCP:192.168.10.60:80
+ssh vpc socat TCP-LISTEN:9000,fork TCP:192.168.10.60:80
 
