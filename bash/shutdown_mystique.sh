b431646554dc207a4a43f5d2c707f373cabd979f
Unmanned User <unmanned@ui-01-080.asv.global>
2017-08-30 18:44:34 +0000

    Updated for field account.

--- bash/shutdown_mystique.sh
+++ bash/shutdown_mystique.sh
@@ -3,6 +3,6 @@
 # A script to shutdown mystique (linux box on CW4)
 
 echo "SHUTTING DOWN MYSTIQUE (Linux PC)"
-echo "Connecting.... password: asvuser"
-ssh -t asvuser@192.168.100.112 'sudo shutdown -h now'
+echo "Connecting.... password for field"
+ssh -t field@192.168.100.112 'sudo shutdown -h now'
 
