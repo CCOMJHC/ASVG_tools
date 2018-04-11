9f1d55d7c70c81754b35d126a81ccb0e35fe293e
Val E. Schmidt <vschmidt@ccom.unh.edu>
2018-04-11 13:23:20 -0400

    download_cw4_data.sh

--- bash/download_cw4_data.sh
+++ bash/download_cw4_data.sh
@@ -5,18 +5,103 @@
 # University of New Hampshire
 # Copyright 2016
 
-echo "Pull data from VS [y/n]?"
+echo ""
+echo "Data from the CW4 is written in the following directory structure:"
+echo "ccscm/"
+echo "     /scm-vp/"
+echo "            /<DATA_DIRECTORY>"
+echo "            /<DATA_DIRECTORY>"
+echo "            /etc."
+echo "" 
+echo "This structure will be maintained in the local copy, repeated"
+echo "for each of the vp and vs. Thus you will get the following:"
+echo ""  
+echo "/vp/ccscm/"
+echo "     /scm-vp/"
+echo "            /<DATA_DIRECTORY>"
+echo "            /<DATA_DIRECTORY>"
+echo "            /etc."
+echo "" 
+echo "/vs/ccscm/"
+echo "     /scm-vp/"
+echo "            /<DATA_DIRECTORY>"
+echo "            /<DATA_DIRECTORY>"
+echo "            /etc."
+echo "" 
+echo "Type the full path where to put the vp/ and vs/ directories below."
+echo "If you already have a data store (e.g you've copied data before"
+echo "and the vp/ccscm/scm-vp/ and/or vs/ccscm/scm-vs/ directories "
+echo "already exist), specify the path to their parent directory"
+echo "If you do not, this structure will be created for you at the"
+echo "location you specify."
+echo -n ">"
+
+read datastore
+mkdir -p $datastore/vp/ccscm/scm-vp
+vpdatastore="$datastore/vp/ccscm/scm-vp"
+mkdir -p $datastore/vs/ccscm/scm-vs
+vsdatastore="$datastore/vs/ccscm/scm-vs"
+
+echo "Pull data from VS too [y/n]?"
 read dovs
-echo "Pull data from VP [y/n]?"
-read dovp
+# Always do the vp
+dovp=y
+
+echo "Pull data over the CW4 Internal [1], Cobham [2] or Wifi [3] Networks?"
+read network
+
+if [ "$network" == 1 ]; then
+	VP="192.168.10.10"
+fi
+
+if [ "$network" == 2 ]; then
+	VP="192.168.100.10"
+fi
+
+if [ "$network" == 3 ]; then
+	VP="192.168.101.10"
+fi
+
+echo "Reading available directories"
+alllogdirs=`ssh unmanned@$VP ls -1 /home/unmanned/ccscm/scm-vp/`
+
+for logdir in ${alllogdirs[@]}; do
+	echo "$logdir"
+done
+
+# Get the user's selection. 
+echo "Type (cut/paste) the directory(s) to pull, with 'Enter' after
+each. Or type '*' to process all of them. Type'q' to quit."
+read datadir
+while [ "$datadir" != 'q' ]; do
+
+    if [ "$datadir" == '*' ]; then
+        datadirs=( ${logswithdata[@]} )
+        datadir="q"
+    else
+
+        datadirs=( "${datadirs[@]}" $datadir )
+        read datadir
+
+    fi # End check for '*'
+
+done
+
+
 
 if [ "$dovs" == 'y' ]; then
 	# Pull data from vs.
-	ssh vs "tar -cpf - ccscm" | pv -rab | tar -xpf - -C /home/unmanned/data/vs/
+        for datadir in ${datadirs[@]}; do
+		ssh unmanned@vs "tar -cpf - ccscm/scm-vs/$datadir" | pv -rab | tar -xpf - -C "$vsdatastore"
+	done
 fi
 
 if [ "$dovp" == 'y' ]; then
 	# Pull data from vp
-	ssh vpc "tar -cpf - ccscm" | pv -rab | \
-		tar -xpf - -C /home/unmanned/data/vp/
+        for datadir in ${datadirs[@]}; do
+
+		echo "Transferring $datadir to $vpdatastore."
+		ssh unmanned@$VP "tar -cpf - -C ccscm/scm-vp/ $datadir" \
+		| pv -rab | tar -xpf - -C "$vpdatastore"
+	done
 fi
